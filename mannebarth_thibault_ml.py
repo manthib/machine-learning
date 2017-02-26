f89c0d405af838c6aa959c0cbc68bec932b0ed71
manthib <thibo_m@hotmail.fr>
2017-02-26 17:25:59 +0100

    add ml file

---
+++ mannebarth_thibault_ml.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier as RandomForest
+from sklearn import datasets
+from sklearn import svm
+
+bc_dataset = datasets.load_breast_cancer()
